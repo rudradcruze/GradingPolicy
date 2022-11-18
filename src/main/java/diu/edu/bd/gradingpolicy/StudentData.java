15e98bc987fc478dd1769fad69f99b30f368b5bf
rudradcruze <francisrudra@gamil.com>
2022-11-18 13:07:55 +0600

    Student all view

---
+++ src/main/java/diu/edu/bd/gradingpolicy/StudentData.java
@@ -0,0 +1,23 @@
+package diu.edu.bd.gradingpolicy;
+
+import java.util.Date;
+
+public class StudentData extends Users {
+
+    private String studentGuardianName;
+    private String studentGuardianPhone;
+
+    public StudentData(String userId, String userName, String userGender, Date userDob, String userPhone, String userSSC, String userHSC, String studentGuardianName, String studentGuardianPhone, String userStatus) {
+        super(userId, userName, userGender, userDob, userPhone, userSSC, userHSC, userStatus);
+        this.studentGuardianName = studentGuardianName;
+        this.studentGuardianPhone = studentGuardianPhone;
+    }
+
+    public String getStudentGuardianName() {
+        return studentGuardianName;
+    }
+
+    public String getStudentGuardianPhone() {
+        return studentGuardianPhone;
+    }
+}
